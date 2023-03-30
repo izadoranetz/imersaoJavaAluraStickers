d8d5b8ffb005a546f6ff913c33ac5c229b357963
izadoranetz <izadora.ns@gmail.com>
2023-03-30 10:58:35 -0300

    inclusao de outra conexao

--- src/App.java
+++ src/App.java
@@ -22,7 +22,7 @@ public class App {
         JsonParser parser = new JsonParser();
         // estrutura associativa - no java chama Map
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        System.out.println(listaDeFilmes.size());
+        System.out.println("\n" + listaDeFilmes.size() + " Top Movies:");
 
         // exibir e manipular dados
         for (Map<String, String> filme : listaDeFilmes) {
@@ -30,10 +30,26 @@ public class App {
             System.out.println(ConsoleColors.WHITE + filme.get("image") + ConsoleColors.RESET );
             System.out.println(ConsoleColors.PURPLE + filme.get("imDbRating") + ConsoleColors.RESET );
         }
-
+        
         // desafios:
         // pintar terminal
         // endpoint: https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json
+        // fazer conexão HTTP e buscar os Top 250 filmes
+        String url2 = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+        URI endereco2 = URI.create(url2);
+        // HttpClient cliente é a mesma coisa que escrever var client (as versões mais novas já entendem o tipo dessa variável)
+        HttpClient client2 = HttpClient.newHttpClient();
+        HttpRequest request2 = HttpRequest.newBuilder(endereco2).GET().build();
+        HttpResponse<String> response2 = client2.send(request2, BodyHandlers.ofString());
+        String body2 = response2.body();
+
+        List<Map<String, String>> listaDeFilmesPopulares = parser.parse(body2);
+        System.out.println("\n" + "Most Popular Movies: ");
+        for (Map<String, String> filme : listaDeFilmesPopulares) {
+            System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT + filme.get("rank") + ConsoleColors.RESET );
+            System.out.println(ConsoleColors.YELLOW_BACKGROUND_BRIGHT + ConsoleColors.BLACK_BOLD_BRIGHT + filme.get("title") + ConsoleColors.RESET );
+            System.out.println(ConsoleColors.PURPLE + filme.get("crew") + ConsoleColors.RESET );
+        }
         // tv: https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json
         // tvPop: https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json
     
